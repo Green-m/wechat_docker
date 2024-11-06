10aad47c410cc2b8a64cd257c1e66b3ef1d3c750
Green-m <root@green-m.me>
2024-11-06 17:33:48 +0800

    Init.

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Wechat docker novnc
+
+
+# Reference
+
+https://github.com/bonigarcia/novnc/tree/master
+
+https://github.com/Attack2Defense/dWeChat/tree/main
+
+
---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -f /tmp/.X0-lock
+/usr/bin/Xvfb :0 -screen 0 1280x960x24 -nolisten unix -ac +extension GLX +extension RENDER &
+export DISPLAY=:0
+
+mkdir /root/wxbot/.vnc/
+touch /root/wxbot/.vnc/passwd
+x11vnc -storepasswd xxxxxx /root/wxbot/.vnc/passwd
+
+export DISPLAY=:0
+x11vnc -forever -display :0 -rfbauth  /root/wxbot/.vnc/passwd &
+fcitx5-configtool
+wechat
